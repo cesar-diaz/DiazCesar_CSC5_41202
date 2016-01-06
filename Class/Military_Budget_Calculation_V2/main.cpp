71b381f4bf78d97472dffb82dc3be9cd972b3a1a
cesar-diaz <cesardiazml@gmail.com>
2016-01-06 08:25:11 -0800

    Signed-off-by: cesar-diaz <cesardiazml@gmail.com>

---
+++ Class/Military_Budget_Calculation_V2/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on January 6, 2016, 8:13 AM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+//Global Constant
+const char cnvPerc = 100; //conversion to percentage
+
+//Function prototype
+
+//Execution begins here
+
+int main(int argc, char** argv) {
+
+    float milBdgt = 600e9f;
+    float fedBdgt = 3.8e12f;
+    float percMil;
+    
+    percMil = milBdgt / fedBdgt * cnvPerc;
+    
+    cout << "The Military budget = $" << milBdgt << endl;
+    cout <<"The Federal budget = " << fedBdgt << endl;
+    cout << " The percentage of the Federal Budget attributed to the "
+            "Military = " << percMil << "%" << endl;
+    
+    return 0;
+}
+
