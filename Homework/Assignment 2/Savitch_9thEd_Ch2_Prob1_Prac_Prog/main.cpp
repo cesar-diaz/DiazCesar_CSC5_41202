e17ceda2781707521405e1680038df1b7f566cfc
cesar-diaz <cesardiazml@gmail.com>
2016-01-12 22:52:50 -0800

    Signed-off-by: cesar-diaz <cesardiazml@gmail.com>

---
+++ Homework/Assignment 2/Savitch_9thEd_Ch2_Prob1_Prac_Prog/main.cpp
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Cesar Diaz
+ * Created on January 12, 2016, 6:59 PM
+ * Purpose: 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+const double TON_OZ = 35273.92;
+
+int main(int argc, char** argv) {
+    
+    double boxWght;
+    unsigned short tries;
+    cout << "How many times would you like to run this program?\n";
+    cin >> tries;
+    while (tries == tries && tries != 0)
+    {
+        cout << "Enter the weight of your cereal box in ounces\n";
+        cin >> boxWght;
+        cout.setf(ios::fixed);
+        cout.setf(ios::showpoint);
+        cout.precision(6);
+        cout << "You have " << boxWght / TON_OZ << " metric tons\n";
+        cout.precision(2);
+        cout << "You would need " << TON_OZ / boxWght << " boxes to make "
+                "a metric ton\n";
+        tries = (tries - 1);
+        cout << "You have " << tries << " tries remaining\n";
+        cout << endl;    
+    }
+    
+    return 0;
+}
+
---
+++ Homework/Assignment 2/Savitch_9thEd_Ch2_Prob3_Prac_Prog/main.cpp
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Cesar Diaz
+ * Created on January 12, 2016, 9:44 PM
+ * Purpose: 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+const short SEC_PER_HR = 3600;//Number of seconds in an hour
+  
+int main(int argc, char** argv) {
+    
+    double miles;//Used double so that the input for miles can be decimals
+    //Prompt the user to enter the desired MPH
+    cout << "Enter the number of miles you would like to run in an hour\n";
+    cin >> miles;
+    int secs = SEC_PER_HR / miles;//Seconds it would take to run each mile
+    int mins;
+    while (secs >=  60)
+    {
+        secs = secs - 60;
+        mins = mins + 1;
+    } 
+    cout << "You would have to run each mile in " << mins << " minutes and " << secs
+                << " seconds to run " << miles << " MPH\n";
+    
+    return 0;
+}
+
