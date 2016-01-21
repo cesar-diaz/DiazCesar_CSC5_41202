93c387f74a8025a54734bf6a555b42df1608c039
cesar-diaz <cesardiazml@gmail.com>
2016-01-21 10:35:52 -0800

    Signed-off-by: cesar-diaz <cesardiazml@gmail.com>

--- Homework/Assignment 3/MenuWithProblemsDecisions/main.cpp
+++ Homework/Assignment 3/MenuWithProblemsDecisions/main.cpp
@@ -43,6 +43,7 @@ int main(int argc, char** argv) {
         cout << "Type 6 to Display Problem Savitch 9th Ed Chap 3 Prob 6\n";
         cout << "Type 7 to Display Problem Savitch 9th Ed Chap 3 Prob 7\n";
         cout << "Type 8 to Display Problem Savitch 9th Ed Chap 3 Prob 10\n";
+        cout << "Type 9 to Display Problem Savitch 9th Ed Chap 3 Prob 3\n";
         
         cout << "Type anything else to exit\n";
         cin >> nSolutn;
@@ -369,6 +370,84 @@ int main(int argc, char** argv) {
                 break;
             }
             case 9:{
+            //The Problem to solve
+            cout << endl << "Soulution to Savitch 9th Ed Chap 3 Prob 3.\n";
+            cout << endl << "The Roman Numeral Conversion Program.\n";
+
+            //Set the random number seed
+           // srand(static_cast<unsigned int>(time(0)));
+
+            //Declare and initialize variables
+            unsigned short number;// = rand() % 2001 + 1000;//[1000,3000]
+            cout << "Enter the year you want to convert to a roman numeral."
+                    "(1000-3000)\n";
+            cin >> number;
+            unsigned char n1000s, n100s, n10s, n1s;//Number of 10^x's
+            cout << "The number to convert = " << number << endl;
+
+            //Calculate the number of 1000s, 100s, 10s, 1s
+            n1000s = (number - number % 1000) / 1000;//Number of 1000s
+            number = (number - n1000s * 1000);//Subtract off 1000s
+            n100s = (number - number % 100) / 100;//Number of 100s
+            number = (number - n100s * 100);//Subtract off 100s
+            n10s = (number - number % 10) / 10;//Number of 10s
+            n1s = (number - n10s * 10);//Subtract off 10s to get 1s
+
+            //Output the number and its components
+            cout << "The number of 1000s = " << static_cast<int>(n1000s) << endl;
+            cout << "The number of 100s = " << static_cast<int>(n100s) << endl;
+            cout << "The number of 10s = " << static_cast<int>(n10s) << endl;
+            cout << "The number of 1s = " << static_cast<int>(n1s) << endl;
+
+            //Output the results
+            cout << "The Roman Numeral = ";
+
+            //Output the 1000s
+            switch(n1000s){
+                case 3: cout << "M";
+                case 2: cout << "M";
+                case 1: cout << "M";
+            }
+
+            //Output the 100s
+            switch(n100s){
+                case 9: cout << "CM"; break;
+                case 8: cout << "DCCC"; break;
+                case 7: cout << "DCC"; break;
+                case 6: cout << "DC"; break;
+                case 5: cout << "D"; break;
+                case 4: cout << "CD"; break;
+                case 3: cout << "C";
+                case 2: cout << "C";
+                case 1: cout << "C";
+            }
+
+            //Output the 10s
+            switch(n10s){
+                case 9: cout << "XC"; break;
+                case 8: cout << "LXXX"; break;
+                case 7: cout << "LXX"; break;
+                case 6: cout << "LX"; break;
+                case 5: cout << "L"; break;
+                case 4: cout << "XL"; break;
+                case 3: cout << "X";
+                case 2: cout << "X";
+                case 1: cout << "X";
+            }
+
+            //Output the 1s
+            switch(n1s){
+                case 9: cout << "IX"; break;
+                case 8: cout << "VIII"; break;
+                case 7: cout << "VII"; break;
+                case 6: cout << "VI"; break;
+                case 5: cout << "V"; break;
+                case 4: cout << "IV"; break;
+                case 3: cout << "I";
+                case 2: cout << "I";
+                case 1: cout << "I";
+            }
+            cout << endl << endl9;
                 break;
             }
             case 10:{
--- Homework/Assignment 3/Savitch_9thEd_Chap3_Prob3_Prog_Proj/nbproject/Makefile-impl.mk
+++ Homework/Assignment 3/Savitch_9thEd_Chap3_Prob3_Prog_Proj/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=HelloWorld
+PROJECTNAME=Savitch_9thEd_Chap3_Prob3_Prog_Proj
 
 # Active Configuration
 DEFAULTCONF=Debug
