40d1ffa406019c8299b95c8f8b408aaef7cd9507
cesar-diaz <cesardiazml@gmail.com>
2016-01-26 11:58:17 -0800

    Signed-off-by: cesar-diaz <cesardiazml@gmail.com>

---
+++ Homework/Assignment 4/Savitch_9thEd_Ch4_Prob5_Prac_Prog/main.cpp
@@ -0,0 +1,60 @@
+/* 
+    File:   main.cpp
+    Author: Cesar Diaz
+    Created on January 26, 2016, 8:33 AM
+    Purpose: Calculate the rate of inflation on an item in the past year.
+ */
+
+
+//system libraries
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+//User Libraries 
+
+//Global Constants
+
+//Function Prototypes 
+
+//Execution Begins Here
+
+int main(int argc, char** argv) {
+    
+    //Declare and initialize variables
+    float oldPrice, nowPrice, rate;
+    char answer;
+    cout << fixed << setprecision(2);
+    
+    do{
+    //Input data
+    cout << "What was the price of your item last year?\n";
+    cin >> oldPrice;//Price that the item used to be
+    cout << "What is the price of the item now?\n";
+    cin >> nowPrice;
+    
+    //Calculate or map inputs to outputs
+    rate = (nowPrice - oldPrice) / oldPrice;
+    
+    //Output the results
+    cout << "The rate of inflation over the past year is " << rate << "%\n";
+    
+    //Calculate price of item in 1 and 2 yrs. from now
+    cout << "If the interest continues at " << rate << " the cost of the item "
+            "will be:\n";
+    nowPrice =  nowPrice + (nowPrice * rate);
+    cout << "In 1 year the price will be:  $" << nowPrice << endl;
+    nowPrice =  nowPrice + (nowPrice * rate);    
+    cout << "In 2 years the price will be: $" << nowPrice << endl;
+    cout << endl << "Would you like to run the program again?\n";
+    cout << "Y - Yes. Anything else - Exit.\n";
+    cin >> answer;
+    }while (toupper(answer) == 'Y');
+    cout << "Exiting the program.\n";
+    
+    //Exit stage right
+
+    return 0;
+}
+
