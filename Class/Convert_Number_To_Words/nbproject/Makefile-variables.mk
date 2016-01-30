1c94d313b148314ac20b390a3a779bda210a4e95
cesar-diaz <cesardiazml@gmail.com>
2016-01-30 00:43:01 -0800

    Signed-off-by: cesar-diaz <cesardiazml@gmail.com>

---
+++ Class/Convert_Number_To_Words/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=convert_number_to_words
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/convert_number_to_words
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=convertnumbertowords.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/convertnumbertowords.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=convert_number_to_words
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/convert_number_to_words
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=convertnumbertowords.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/convertnumbertowords.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
